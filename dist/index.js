fc2671585e4619da146688dee41d57c11af93c5b
Dotan Simha <dotansimha@gmail.com>
2022-08-04 10:08:09 +0300

    added bundle size info

--- dist/index.js
+++ dist/index.js
@@ -33974,7 +33974,7 @@ function formatTable(packages) {
 |------|---------|----|`;
   return `${header}
 ${packages.map(
-    (t) => `| \`${t.name}\` | \`${t.version}\` | [npm \u2197\uFE0E](https://www.npmjs.com/package/${t.name}/v/${t.version}) [unpkg \u2197\uFE0E](https://unpkg.com/browse/${t.name}@${t.version}/) |`
+    (t) => `| \`${t.name}\` | \`${t.version}\` | [npm \u2197\uFE0E](https://www.npmjs.com/package/${t.name}/v/${t.version}) [unpkg \u2197\uFE0E](https://unpkg.com/browse/${t.name}@${t.version}/) ![npm bundle size](https://img.shields.io/bundlephobia/min/${t.name}/${t.version}?label=bundle%20size) |`
   ).join("\n")}`;
 }
 async function upsertComment(options) {
